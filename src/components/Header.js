38e2190174525681907a318be1a625b32c001bc3
Angelo Rubin <angelorubin@gmail.com>
2022-07-29 15:06:49 -0300

    Renomeando componente Link pra evitar conflito

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,63 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Box, Typography, Button } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Box, Typography, Button, Link as MuiLink } from "@mui/material";
 import Cart from "./cart";
+import { useTheme } from "@emotion/react";
 
 const Header = () => {
+	const theme = useTheme();
+	const { palette, spacing } = theme;
+	const { common, primary, secondary } = palette;
+
 	return (
-		<Box>
-			<Typography variant="h3">Dio Shopping</Typography>
-			<Link to="/">
-				<Button color="primary">Home</Button>
-			</Link>
-			<Link to="/contato">
-				<Button color="primary">Contato</Button>
-			</Link>
-			<Cart />
+		<Box
+			sx={{
+				display: "flex",
+				alignItems: "center",
+				height: "15vh",
+				backgroundColor: primary.main,
+				padding: spacing(2),
+			}}
+		>
+			<Box sx={{ flex: 1 }}>
+				<Typography sx={{ fontWeight: "900" }} variant="h4">
+					Dio Shopping
+				</Typography>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					gap: spacing(2),
+					flex: 1,
+				}}
+			>
+				<Typography>
+					<MuiLink
+						to="/"
+						component={RouterLink}
+						sx={{ color: common.white, textDecoration: "none" }}
+					>
+						Home
+					</MuiLink>
+				</Typography>
+
+				<MuiLink
+					to="/contato"
+					component={RouterLink}
+					sx={{ color: common.white, textDecoration: "none" }}
+				>
+					<Typography>Contato</Typography>
+				</MuiLink>
+			</Box>
+			<Box
+				sx={{
+					border: `1px solid ${common.white}`,
+					flex: 1,
+					justifyContent: "flex-end",
+				}}
+			>
+				<Cart />
+			</Box>
 		</Box>
 	);
 };
