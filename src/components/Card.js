c6285f0ca436de56281c25cd26d8f1bcae74b9d5
Angelo Rubin <angelorubin@gmail.com>
2022-07-29 15:05:47 -0300

    Trocando componentes de Grid por Box

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,32 +1,51 @@
 import React from "react";
-import { Paper, Grid, Typography, Button, makeStyles } from "@mui/material";
+import { Paper, Typography, Button, Box, useTheme } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
 import cartActions from "components/store/actions/cart";
 
 export default ({ product, children }) => {
 	const cart = useSelector((state) => state.cart.value);
 	const dispatch = useDispatch();
+	const theme = useTheme();
+	const { palette, spacing } = theme;
+
+	const handleClick = () => {
+		return dispatch(cartActions.Add(cart, product));
+	};
 
 	return (
-		<Grid item xs={3}>
-			<Paper>
-				<Grid>
-					<Grid item>
-						<img width="140px" src={product.image} alt={product.name_product} />
-						<Typography variant="h6">{children}</Typography>
-						<Typography variant="subtitle1">
-							R$ {product.price.toFixed(2)}
-						</Typography>
-					</Grid>
+		<Box
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				flex: 1,
+				alignItems: "center",
+			}}
+		>
+			<Box
+				sx={{
+					display: "flex",
+					flex: 1,
+					flexDirection: "column",
+				}}
+			>
+				<Box
+					component="img"
+					width="140px"
+					src={product.image}
+					alt={product.name_product}
+				/>
+				<Typography sx={{ textAlign: "center" }} variant="h6">
+					{children}
+				</Typography>
+				<Typography sx={{ textAlign: "center" }} variant="subtitle1">
+					R$ {product.price.toFixed(2)}
+				</Typography>
+			</Box>
 
-					<Button
-						variant="contained"
-						onClick={() => dispatch(cartActions.Add(cart, product))}
-					>
-						Adicionar
-					</Button>
-				</Grid>
-			</Paper>
-		</Grid>
+			<Button fullWidth variant="contained" onClick={handleClick}>
+				Adicionar
+			</Button>
+		</Box>
 	);
 };
