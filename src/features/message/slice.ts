b1687d9ba4cadce6b2a4a9b6e915d202cf887f33
angelorubin <angelorubin@gmail.com>
2022-08-07 11:15:42 -0300

    Implementando feature message

--- src/features/message/slice.ts
+++ src/features/message/slice.ts
@@ -3,34 +3,44 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 import http from "axios";
 
 export interface MessagesState {
-	data: [];
+  data: [];
+  isLoading: boolean;
+  status: string;
 }
 
 export const getMessages = createAsyncThunk(
-	"messages/getMessages",
-	async () => {
-		const response = await http("http://localhost:5000/messages");
-		return response.data;
-	}
+  "messages/getMessages",
+  async () => {
+    const response = await http("http://localhost:5000/messages");
+    return response.data;
+  }
 );
 
 const initialState: MessagesState = {
-	data: [],
+  data: [],
+  isLoading: false,
+  status: "idle",
 };
 
 export const messagesSlice = createSlice({
-	name: "messages",
-	initialState,
-	reducers: {
-		x: (state, action: PayloadAction<string>) => {
-			return state;
-		},
-	},
-	extraReducers(builder) {
-		builder.addCase(getMessages.fulfilled, (state, action) => {
-			return state;
-		});
-	},
+  name: "messages",
+  initialState,
+  reducers: {
+    x: (state, action: PayloadAction<string>) => {
+      return state;
+    },
+  },
+  extraReducers(builder) {
+    builder.addCase(getMessages.pending, (state, action) => {
+      state.status = "pending";
+      state.isLoading = true;
+    });
+    builder.addCase(getMessages.fulfilled, (state, action) => {
+      state.status = "fullfiled";
+      state.data = action.payload;
+      state.isLoading = false;
+    });
+  },
 });
 
 // Action creators are generated for each case reducer function
