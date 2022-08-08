f19e24529f4b02f21d6da3e139434301dd011021
Angelo Rubin <angelorubin@gmail.com>
2022-08-08 17:55:18 -0300

    Modificando nome do estado de data para mensagens

--- src/features/message/slice.ts
+++ src/features/message/slice.ts
@@ -1,44 +1,44 @@
-import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { http } from "config/api";
 
 export interface MessagesState {
-  data: [];
-  isLoading: boolean;
-  status: string;
+	messages: [];
+	isLoading: boolean;
+	status: string;
 }
 
 export const getMessages = createAsyncThunk(
-  "messages/getMessages",
-  async () => {
-    const response = await http("/messages");
-    return response.data;
-  }
+	"messages/getMessages",
+	async () => {
+		const response = await http("/messages");
+		return response.data;
+	}
 );
 
 const initialState: MessagesState = {
-  data: [],
-  isLoading: false,
-  status: "idle",
+	messages: [],
+	isLoading: false,
+	status: "idle",
 };
 
 export const messagesSlice = createSlice({
-  name: "messages",
-  initialState,
-  reducers: {},
-  extraReducers(builder) {
-    builder.addCase(getMessages.pending, (state, action) => {
-      state.status = "pending";
-      state.isLoading = true;
-    });
-    builder.addCase(getMessages.fulfilled, (state, action) => {
-      state.status = "fullfiled";
-      state.data = action.payload;
-      state.isLoading = false;
-    });
-  },
+	name: "messages",
+	initialState,
+	reducers: {},
+	extraReducers(builder) {
+		builder.addCase(getMessages.pending, (state, action) => {
+			state.status = "pending";
+			state.isLoading = true;
+		});
+		builder.addCase(getMessages.fulfilled, (state, action) => {
+			state.status = "fullfiled";
+			state.messages = action.payload;
+			state.isLoading = false;
+		});
+	},
 });
 
 // Action creators are generated for each case reducer function
-export const {} = messagesSlice.actions;
+// export const {} = messagesSlice.actions;
 
 export default messagesSlice.reducer;
