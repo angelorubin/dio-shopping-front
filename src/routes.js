3f2f36cc2e129550919502b10ba84e3c8f55a4c6
angelorubin <angelorubin@gmail.com>
2022-08-07 22:59:44 -0300

    Ajustando rotas

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ import Home from "pages/home";
 import Contact from "pages/contact";
 
 const MainRoutes = () => {
-	return (
-		<Routes>
-			<Route path="*" element={<h1>Page not found.</h1>} />
-			<Route path="/home" element={<Home />} />
-			<Route index path="/" element={<Contact />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="*" element={<h1>Page not found.</h1>} />
+      <Route index path="/home" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 };
 
 export default MainRoutes;
