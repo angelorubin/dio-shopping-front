ce1e907ee88797874afa5b9422c58b49095af0d3
Angelo Rubin <angelorubin@gmail.com>
2022-08-08 17:52:33 -0300

    Modificndo a cor do logo

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -9,57 +9,60 @@ const Header = () => {
   const { common, primary } = palette;
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        height: "15vh",
-        backgroundColor: primary.main,
-        padding: spacing(2),
-      }}
-    >
-      <Box sx={{ flex: 1 }}>
-        <Typography sx={{ fontWeight: "900" }} variant="h4">
-          Dio Shopping
-        </Typography>
-      </Box>
+		<Box
+			sx={{
+				display: "flex",
+				alignItems: "center",
+				height: "15vh",
+				backgroundColor: primary.main,
+				padding: spacing(2),
+			}}
+		>
+			<Box sx={{ flex: 1 }}>
+				<Typography
+					sx={{ fontWeight: "900", color: common.white }}
+					variant="h4"
+				>
+					Dio Shopping
+				</Typography>
+			</Box>
 
-      <Box
-        sx={{
-          display: "flex",
-          gap: spacing(2),
-          flex: 1,
-        }}
-      >
-        <Typography>
-          <MuiLink
-            to="/home"
-            component={RouterLink}
-            sx={{ color: common.white, textDecoration: "none" }}
-          >
-            Home
-          </MuiLink>
-        </Typography>
+			<Box
+				sx={{
+					display: "flex",
+					gap: spacing(2),
+					flex: 1,
+				}}
+			>
+				<Typography>
+					<MuiLink
+						to="/home"
+						component={RouterLink}
+						sx={{ color: common.white, textDecoration: "none" }}
+					>
+						Home
+					</MuiLink>
+				</Typography>
 
-        <MuiLink
-          to="/contact"
-          component={RouterLink}
-          sx={{ color: common.white, textDecoration: "none" }}
-        >
-          <Typography>Contato</Typography>
-        </MuiLink>
-      </Box>
+				<MuiLink
+					to="/contact"
+					component={RouterLink}
+					sx={{ color: common.white, textDecoration: "none" }}
+				>
+					<Typography>Contato</Typography>
+				</MuiLink>
+			</Box>
 
-      <Box
-        sx={{
-          flex: 1,
-          justifyContent: "flex-end",
-        }}
-      >
-        <Cart />
-      </Box>
-    </Box>
-  );
+			<Box
+				sx={{
+					flex: 1,
+					justifyContent: "flex-end",
+				}}
+			>
+				<Cart />
+			</Box>
+		</Box>
+	);
 };
 
 export default Header;
